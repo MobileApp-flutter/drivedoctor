93c3c8f53c43e2f8c86a62c7c6eb9fe73b600c08
TheresaLau <theresa@graduate.utm.my>
2023-05-27 19:03:56 +0800

    DRD-13 added search options in dashboard.dart car service and shop now in dashboard.dart display data from firebase

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,13 +1,30 @@
 import 'package:drivedoctor/bloc/controller/auth.dart';
 import 'package:drivedoctor/bloc/models/user.dart';
+import 'package:drivedoctor/bloc/models/shop.dart';
+import 'package:drivedoctor/bloc/models/product.dart';
+import 'package:drivedoctor/bloc/controller/shopController.dart';
+import 'package:drivedoctor/bloc/controller/productController.dart';
+
 import 'package:flutter/material.dart';
 import '../../widgets/bottom_navigation_bar.dart';
 
-class DashboardPage extends StatelessWidget {
+class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
 
   @override
+  _DashboardPageState createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  String searchOption = 'all'; // Add searchOption variable
+  List<ShopData> shops = [];
+  List<ProductData> products = [];
+
+  @override
   Widget build(BuildContext context) {
+    final ShopController _shopController = ShopController();
+    final ProductController _productController = ProductController();
+
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -64,14 +81,28 @@ class DashboardPage extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: const Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
                   ),
-                ),
+                  const SizedBox(width: 10),
+                  // Add Radio buttons for search options
+                  Row(
+                    children: [
+                      buildSearchOption('All', 'all'),
+                      buildSearchOption('Shop', 'shop'),
+                      buildSearchOption('Product', 'product'),
+                    ],
+                  ),
+                ],
               ),
             ),
             Padding(
@@ -98,40 +129,58 @@ class DashboardPage extends StatelessWidget {
             SizedBox(height: 10.0),
             Container(
               height: 200.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 3,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    margin: EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(
-                          'https://soyacincau.com/wp-content/uploads/2021/08/210805-GoCar_garage-1.jpg',
-                          height: 100.0,
-                          width: 150.0,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Car Service Title',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+              child: FutureBuilder<List<ShopData>>(
+                future: _shopController.getShops(), // Retrieve shops data
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    final shops = snapshot.data;
+                    if (shops != null && shops.isNotEmpty) {
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: shops.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final shop = shops[index];
+                          return Card(
+                            margin: EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  shop.imageUrl,
+                                  height: 100.0,
+                                  width: 150.0,
+                                  fit: BoxFit.cover,
                                 ),
-                              ),
-                              Text('Rating: 4.5'),
-                              Text('Shop Name'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        shop.shopname,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text('Rating: ${shop.rating}'),
+                                      Text(shop.companyname),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return Text('No car services available');
+                    }
+                  }
                 },
               ),
             ),
@@ -159,57 +208,122 @@ class DashboardPage extends StatelessWidget {
             SizedBox(height: 8.0),
             Container(
               height: 200.0,
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: 4,
-                itemBuilder: (BuildContext context, int index) {
-                  return Card(
-                    margin: EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Image.network(
-                          'https://soyacincau.com/wp-content/uploads/2021/08/210805-GoCar_garage-1.jpg',
-                          height: 120.0,
-                          width: 200.0,
-                          fit: BoxFit.cover,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Product Title',
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
+              child: FutureBuilder<List<ProductData>>(
+                future:
+                    _productController.getProducts(), // Retrieve products data
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  } else if (snapshot.hasError) {
+                    return Text('Error: ${snapshot.error}');
+                  } else {
+                    print('Snapshot data: ${snapshot.data}');
+                    final products = snapshot.data;
+                    if (products != null && products.isNotEmpty) {
+                      return ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: products.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          final product = products[index];
+                          return Card(
+                            margin: EdgeInsets.all(8.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Image.network(
+                                  product.imageUrl,
+                                  height: 120.0,
+                                  width: 200.0,
+                                  fit: BoxFit.cover,
                                 ),
-                              ),
-                              Text('Price: \$100'),
-                              Text('Shop Name'),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+                                Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        product.productName,
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        'Price: \$${product.price.toStringAsFixed(2)}',
+                                      ),
+                                      FutureBuilder<ShopData?>(
+                                        future: _shopController
+                                            .getShopById(product.shopId),
+                                        builder: (context, snapshot) {
+                                          if (snapshot.connectionState ==
+                                              ConnectionState.waiting) {
+                                            return CircularProgressIndicator();
+                                          } else if (snapshot.hasError) {
+                                            return Text('Error fetching shop');
+                                          } else {
+                                            final shop = snapshot.data;
+                                            if (shop != null) {
+                                              return Text(shop.shopname);
+                                            } else {
+                                              return Text('Shop not found');
+                                            }
+                                          }
+                                        },
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return Text('No products available');
+                    }
+                  }
                 },
               ),
             ),
             SizedBox(height: 16.0),
             Align(
               alignment: Alignment.center,
-              child: ElevatedButton(
+              child: ElevatedButton.icon(
                 onPressed: () {
-// Go to MyShop button clicked
+                  // Go to MyShop button clicked
                 },
-                child: Text('Go to MyShop'),
+                icon: Icon(Icons.shop), // Add the shop icon
+                label: Text('Go to My Shop'),
               ),
             ),
             SizedBox(height: 16.0),
           ],
         ),
       ),
+    );
+  }
+
+  Widget buildSearchOption(String text, String value) {
+    final isSelected = searchOption == value;
+    return Row(
+      children: [
+        Radio<String>(
+          value: value,
+          groupValue: searchOption,
+          onChanged: (String? newValue) {
+            setState(() {
+              searchOption = newValue!;
+            });
+          },
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+            color: isSelected ? Colors.blue : Colors.black,
+          ),
+        ),
+      ],
     );
   }
 }
